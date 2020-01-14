d90520702342111b5b385131bb04ee89b8b7ce12
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2020-01-14 18:18:13 +0100

    feat: item detail

--- src/components/PhoneDetail.js
+++ src/components/PhoneDetail.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import phones from '../phones.json'
 
-const PhoneDetail = () => (
-  <div className="PhoneDetail" style={{ marginTop: '100px' }}>
-    <div className="container">
-      <div className="row min-vh-100 justify-content-center">
-        <div className="col-4">
-          <img className="w-100" alt="..." src="/images/galaxy-a70.jpg" />
-        </div>
-        <div className="col-4">
-          <h1 className="text-left">Galaxy bla bla</h1>
+const PhoneDetail = (props) => {
+  const phone = phones.find(p => p.id === props.match.params.id)
+
+  return (
+    <div className="PhoneDetail" style={{ marginTop: '100px' }}>
+      <div className="container">
+        <div className="row min-vh-100 justify-content-center">
+          <div className="col-4">
+            <img className="w-100" alt="..." src={phone.img} />
+          </div>
+          <div className="col-4">
+            <h1 className="text-left">{phone.name}</h1>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default PhoneDetail
